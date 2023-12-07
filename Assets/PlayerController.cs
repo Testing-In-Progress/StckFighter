62f3a95939c62ec4f239b2ab2b834d64e1a720ab
gl91306@student.musd.org <gl91306@student.musd.org>
2023-12-06 16:01:38 -0800

    Co-authored-by: Aman Nindra <amannindra@users.noreply.github.com>

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class P : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    
+    
+    public static int playNum;
+    
+    [SerializeField] float jump;
+    [SerializeField] float speed;
     void Start()
     {
-        
+        jump = 3f;
+        speed = 8f;
     }
-
     // Update is called once per frame
     void Update()
     {
         
-    }
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontalInput, verticalInput) * speed * Time.deltaTime;
+        transform.Translate(movement);
+
+            }
 }
+        
