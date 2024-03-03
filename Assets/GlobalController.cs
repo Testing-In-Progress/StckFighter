dd3d359f2d41f562870c7c0d6a0361d275d6fd85
LittleSA1GON <ethanhsvu@gmail.com>
2024-03-02 21:13:27 -0800

    e

--- Assets/GlobalController.cs
+++ Assets/GlobalController.cs
@@ -52,7 +52,7 @@ public class GlobalController : MonoBehaviour
         };
         Andre.hitGround = () => {
             
-        }
+        };
 
         // FLLFFL code
         FLLFFL = new CharacterBase();
